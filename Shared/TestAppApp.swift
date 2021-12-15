0ae67ccee8a94abb55a576576546405c6f6ca19a
Justin Lange <langejustin@icloud.com>
2021-12-15 23:17:01 +0000

    Initial Commit

---
+++ Shared/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  Shared
+//
+//  Created by Justin Lange on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
